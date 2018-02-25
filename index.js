ed8380ee4ae309846ab60eec7a4ef70608c371de
Himanshu Shekhar <himanshushekharb16@gmail.com>
2018-02-25 07:50:13 +0530

    restructuring to fix error caused in db access

--- index.js
+++ index.js
@@ -6,57 +6,64 @@ var MongoClient = require('mongodb').MongoClient,
     trackers = require('./trackers'),
     json2csv = require('json2csv').parse
 
-var db = null
 var app = express()
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 const default_page_size = Number(process.env.PAGE_SIZE)
 
 async function setup_database() {
-    db = (await MongoClient.connect(process.env.DB_URL)).db(process.env.DB_NAME)
+    let db = (await MongoClient.connect(process.env.DB_URL)).db(process.env.DB_NAME)
     return db
 }
 
-app.post('/add_tracker', (req, res) => {
-    trackers.newTracker(req, res, db)
-})
+function setup_routes(db) {
 
-app.delete('/remove_tracker', (req, res) => {
-    trackers.removeTracker(req, res, db)
-})
+    app.get('/', (req, res) => {
+        res.sendFile(`${__dirname}/index.html`)
+    })
 
-app.get('/list_trackers', (req, res) => {
-    trackers.listTrackers(req, res, db)
-})
+    app.post('/add_tracker', (req, res) => {
+        trackers.newTracker(req, res, db)
+    })
 
-app.post('/find', async (req, res) => {
-    let format = req.body.format
-    let query = req.body.query
+    app.delete('/remove_tracker', (req, res) => {
+        trackers.removeTracker(req, res, db)
+    })
 
-    let page_no = (req.body.page_no === undefined) ? 1 : (req.body.page_no)
-    let page_size = (req.body.page_size === undefined) ? default_page_size : req.body.page_size
-    let projection = (req.body.projection === undefined) ? {} : req.body.projection
+    app.get('/list_trackers', (req, res) => {
+        trackers.listTrackers(req, res, db)
+    })
 
-    if (format === undefined) {
-        res.status(400).json({
-            message: 'Missing format or query'
-        })
-        return
-    }
+    app.post('/find', async (req, res) => {
+        let format = req.body.format
+        let query = req.body.query
 
-    let tweets = await twitter.searchTweets(query, projection, db, page_no, page_size)
-    if (format == 'json') {
-        res.json(tweets)
-    } else if (format == 'csv') {
-        res.set('Content-Type', 'application/octet-stream')
-        res.send(new Buffer(json2csv(tweets, {flatten: true})))
-    }
-})
+        let page_no = (req.body.page_no === undefined) ? 1 : (req.body.page_no)
+        let page_size = (req.body.page_size === undefined) ? default_page_size : req.body.page_size
+        let projection = (req.body.projection === undefined) ? {} : req.body.projection
+
+        if (format === undefined) {
+            res.status(400).json({
+                message: 'Missing format or query'
+            })
+            return
+        }
+
+        let tweets = await twitter.searchTweets(query, projection, db, page_no, page_size)
+        if (format == 'json') {
+            res.json(tweets)
+        } else if (format == 'csv') {
+            res.set('Content-Type', 'application/octet-stream')
+            res.send(new Buffer(json2csv(tweets, { flatten: true })))
+        }
+    })
+}
 
 async function main() {
-    await setup_database()
+    let db = await setup_database()
     trackers.setupKnownStreams(db)
+    setup_routes(db)
     app.listen(5000)
 }
 
